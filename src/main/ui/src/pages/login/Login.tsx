871d409381b598916708d55b3c7e375e45cc3cb8
Ian Robinson <robinsonir02@gmail.com>
2023-08-20 21:18:01 -0400

    updated login/signup UI and implemented register

--- src/main/ui/src/pages/login/Login.tsx
+++ src/main/ui/src/pages/login/Login.tsx
@@ -6,19 +6,21 @@ export const Login = () => {
     return (
 
         <div className="login-form">
-            <h2>Login</h2>
-            <form>
-                {/* Form fields go here */}
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" name="email" />
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" />
-                </div>
-                {/* Other form fields */}
-                <button type="submit">Login</button>
-            </form>
-            <a href="/signup">Need an account? Register here</a>
+            <div className="login-form-container">
+                <h2>Login</h2>
+                <form>
+                    {/* Form fields go here */}
+                    <div className="form-group">
+                        <label htmlFor="email">Email/Username</label>
+                        <input type="text" id="email" name="email" />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" />
+                    </div>
+                    {/* Other form fields */}
+                    <button type="submit">Login</button>
+                </form>
+                <a href="/signup">Need an account? Register here</a>
+            </div>
         </div>
     );
 };
