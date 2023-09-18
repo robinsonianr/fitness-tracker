9c4621e670ec9352b3f18f3da7585e968760e5b8
Ian Robinson <robinsonir02@gmail.com>
2023-09-18 12:47:52 -0400

    codacy fix

--- backend/src/main/java/com/robinsonir/fitnesstracker/customer/CustomerService.java
+++ backend/src/main/java/com/robinsonir/fitnesstracker/customer/CustomerService.java
@@ -24,6 +24,7 @@ public class CustomerService {
     private final CustomerDTOMapper customerDTOMapper;
 
     private final PasswordEncoder passwordEncoder;
+
     private final S3Service s3Service;
 
 
