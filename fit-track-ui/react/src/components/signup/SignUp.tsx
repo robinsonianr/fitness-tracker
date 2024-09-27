36c2018efea789f58b41fd63de34334612bf9ca3
Ian Robinson <robinsonir02@gmail.com>
2024-09-27 12:30:04 -0400

    codacy fixes

--- fit-track-ui/react/src/components/signup/SignUp.tsx
+++ fit-track-ui/react/src/components/signup/SignUp.tsx
@@ -82,7 +82,7 @@ export const SignUp = () => {
                         <label htmlFor="name">Full Name</label>
                         <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
                         <label htmlFor="email">Email</label>
-                        <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}/>
+                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" name="password" value={formData.password} 
                             onChange={handleChange}/>
